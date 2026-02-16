c1b8c3ab9801afcb6891dfd699f81b9469b0d843
Andrew Calimlim <andr3wcalimlim@gmail.com>
2026-02-15 16:20:52 -0800

    Default med data class fully documented

--- DefaultMedicationData.java
+++ DefaultMedicationData.java
@@ -5,18 +5,59 @@ import java.io.File;
 
 public class DefaultMedicationData { 
 	
-	// Contains variables used as backup data in case exceptions occur
-	// in the process of saving and loading from local data files
+	/*
 
-	// Meant to avoid disruption of back-end functionality
+	
+	PAGE	TYPE		VARIABLE
 
-	// Backup data is derived from a JSON file locally saved
-	// titled "DefaultMedication.json"
-	// and it represents the alternative/supplement to ADHD meds that I use:
-	// Caffeine, baby!
+	1	File		DEFAULT_MEDICATION_FILE
 
-	// TODO
-	// Rename: DEFAULT_MEDICATION_FILE
+	2	Medication	DEFAULT_MEDICATION
+
+	3	String		DEFAULT_JSON_STRING
+
+	4	JsonObject	DEFAULT_JSON_OBJECT
+	
+	
+	
+	This class constains backup data variables meant to be used
+	when unchecked exceptions are raised during the typical save
+	and load process.
+
+	The intent is to avoid disruption of back-end
+	functionality (i.e. system crashes when the front-end UI is running).
+
+	Backup data is derived from the locally saved JSON Medication file
+	"DefaultMedication.json" representing the supplement/alternative to
+	ADHD medication that I typically use--caffeine, baby!
+
+	Note that this default Medication file is not counted as a Medication
+	file or treated as such as it should only be displayed and used 
+	in the case of a save or load error.
+			
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+				     Page 0
+
+								    CTRL + F -->
+*/
+
+
+	// DEFAULT MEDICATION FILE //
 
 	protected static final File DEFAULT_MEDICATION_FILE;
 
@@ -48,6 +89,32 @@ public class DefaultMedicationData {
 	"Returning default Medication file as an alternative.";
 	
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+				     Page 1
+
+<-- CTRL + B							    CTRL + F -->
+*/
+
+
+	// DEFAULT MEDICATION //
+
 	protected static final Medication DEFAULT_MEDICATION;
 
 	static{
@@ -76,6 +143,34 @@ public class DefaultMedicationData {
 
 	protected static final String DEFAULT_MEDICATION_ERROR_MESSAGE_SUFFIX = 
 	"Returning the default Medication instance as an alternative.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+				     Page 2
+
+<-- CTRL + B							    CTRL + F -->
+*/
+
+
+	// DEFAULT JSON-FORMATTED STRING //
 
 	protected static final String DEFAULT_JSON_STRING;
 
@@ -108,6 +203,32 @@ public class DefaultMedicationData {
 	"Medication instance as an alternative.";
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+				     Page 3
+
+<-- CTRL + B							    CTRL + F -->
+*/
+
+
+	// DEFAULT JSON OBJECT //
+
 	protected static final JsonObject DEFAULT_JSON_OBJECT; 
 
 	static{
@@ -135,5 +256,31 @@ public class DefaultMedicationData {
 	protected static final String DEFAULT_JSON_OBJECT_MESSAGE_SUFFIX = 
 	"Returning a JSONObject hashmap representing the default Medication " + 
 	"instance as an alternative.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+				     Page 4
+
+<-- CTRL + B							    
+*/
 	
 }
