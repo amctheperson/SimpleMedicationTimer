4e79852f5286a8317068d029c327bbc883f54f87
Andrew Calimlim <andr3wcalimlim@gmail.com>
2026-03-19 17:18:43 -0700

    archiving last modified time method made in Test

--- Test.java
+++ Test.java
@@ -1,54 +1,76 @@
+import com.github.cliftonlabs.json_simple.JsonObject;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
+import java.time.ZoneOffset;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class Test{
 
-	public static void main(String[] args) throws Exception{
-				
-		Medication testMed1 = 	new Medication(
-						"Adderall",
-						"20mg",
-						"XR",
-						3.0
-					);
+	public static String getLastModifiedString(File file) throws Exception{
 
-		Medication testMed2 = 	new Medication(
-						"Adderall",
-						"10mg",
-						"IR",
-						2.0
-					);
+		long mod_epoch_milliseconds = file.lastModified();
+		long mod_epoch_seconds = mod_epoch_milliseconds / 1000;
 
-		LocalDateTime right_now = 
-		LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+		LocalDateTime mod_LDT = 
+		LocalDateTime.ofEpochSecond(
+			
+			mod_epoch_seconds,
+			0,
+			ZoneOffset.of("-7")
+		);
 	
-		Medication[] testDailyRoutine = {testMed1, testMed2};
+		String mod_string = mod_LDT.format(UserData.DISPLAY_FORMAT);	
+
+		return mod_string;
+
+	}
 
 
-		User testUser = new User(
-				"Andrew",
-				testDailyRoutine
+	public static void main(String[] args) throws Exception{
+
+		File userFile = UserData.USER_FILE;
+		
+		String firstModifiedTime = getLastModifiedString(userFile); 
+
+		Medication testMed1 =
+		new Medication(
+	
+			"Adderall",
+			"20 mg",
+			"XR",
+			3.0
+
 		);
 
-		testUser.getWhenLastTaken().put(testMed1, right_now);
-		testUser.getWhenLastTaken().put(testMed2, right_now);
+		Medication testMed2 =
+		new Medication(
+	
+			"Adderall",
+			"10 mg",
+			"IR",
+			2.0
 
-		HashMap<String,String> test_serialized_WLT = 
-		UserData.serializeWhenLastTaken(testUser);				
-		for (HashMap.Entry<String,String> serialized_pair : 
-		test_serialized_WLT.entrySet()){
+		);
 
-			String serialized_med = serialized_pair.getKey();
-			String serialized_ldt = serialized_pair.getValue();
 
-			System.out.println("(" + serialized_med + " , " + 
-			serialized_ldt + ")"); 		
+		Medication[] testDailyRoutine = {testMed1, testMed2};				
+		User newUser = new User("Andrew", testDailyRoutine);
+ 	
+		UserData.saveUserToJsonFile(newUser);
+		
+		User loadedNewUser = UserData.loadUserFromJsonFile(); 
 
-		}	
-			
+		String secondModifiedTime = getLastModifiedString(userFile);
+
+		//System.out.println(oldUser);	
+		System.out.println(firstModifiedTime);
+		System.out.println(loadedNewUser);
+		System.out.println(secondModifiedTime);		
+		
 	}
 }
